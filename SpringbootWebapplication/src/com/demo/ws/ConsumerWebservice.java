ac2cba11d470293804996663f5f14b67d409be6d
RangaiahB <rangaiah.bakkathatla@gmail.com>
2018-09-08 22:29:36 +0530

    demchanges

---
+++ SpringbootWebapplication/src/com/demo/ws/ConsumerWebservice.java
@@ -0,0 +1,15 @@
+package com.demo.ws;
+
+public class ConsumerWebservice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+        RestTemplate restTemplate = new RestTemplate();
+        Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+        log.info(quote.toString());
+
+
+	}
+
+}
